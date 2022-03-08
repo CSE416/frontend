2fd538b077241f66a276f4ee1815da3a64f304a6
Sudara Ranasinghe <sudara1999@gmail.com>
2022-03-08 04:21:41 -0500

    select-state works

--- src/App.js
+++ src/App.js
@@ -6,38 +6,59 @@ import './styles/planSummary.css';
 import { AddDistricting } from './components/AddDistricting';
 import { AddGraph } from './components/AddGraph';
 import { InformationTab } from './components/InformationTab';
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import './utils/window_size';
-import useWindowDimensions from './utils/window_size';
-import React, { useState } from 'react';
-import { display } from '@mui/system';
-import gdata from './gz_2010_us_040_00_20m.json'
+import React, { useEffect, useState, useRef } from 'react';
+import USstatesGJSONdata from './gz_2010_us_040_00_20m.json'
 import Stack from '@mui/material/Stack';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const { _height, _width } = useWindowDimensions();
   const [isSplit, setIsSplit] = useState(true);
-  const [currState, setCurrState] = useState('null');
   const [planId, setPlanId] = useState(0);
   const [planName, setPlanName] = useState('');
+  const [currUSstate, setCurrUSstate] = useState(null);
+  const [USmap, setUSMap] = useState(null);
+  const [USstatesGJSON, setUSstatesGJSON] = useState(null);
+  const mapGJSONref = useRef();
+
+  useEffect(() => {
+    // Get GEOJSON data for the US states
+    // GET /us_states
+    setUSstatesGJSON(USstatesGJSONdata);
+  }, []);
+
+  useEffect(() => {
+    if (USmap && mapGJSONref.current){
+      // if a state has been selected, zoom in on the state
+      if (currUSstate != null){
+        USmap.fitBounds(mapGJSONref.current.getLayers().find((layer) => layer.feature.properties.STATE == currUSstate.fipsCode).getBounds());
+      }
+      // else show US map
+      else{
+
+      }
+    }
+  }, [currUSstate]);
 
   return (
     <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
       <div className="menu-bar" style={{ display: 'flex' }}>
-        <Autocomplete
-          onChange={(e, v) => setCurrState(v)}
+        {mapGJSONref.current && <Autocomplete
+          onChange={(event, value) => {setCurrUSstate(value?{name: value.name, fipsCode: value.fipsCode}:null)}}
           id="state-select"
-          sx={{ width: '15em', margin: '0.5em' }}
-          options={['Nevada', 'S. Carolina']}
+          sx={{ width: '14em', margin: '0.5em' }}
+          options={mapGJSONref.current.getLayers().map((layer) => ({name: layer.feature.properties.NAME, fipsCode: layer.feature.properties.STATE}))}
           autoHighlight
+          autoComplete
+          getOptionLabel={(option) => option.name}
+          isOptionEqualToValue={(option, value) => parseInt(option.fipsCode) == parseInt(value.fipsCode)}
           renderOption={(props, option) => (
             <Box component="li" {...props}>
-              {option}
+              {option.name}
             </Box>
           )}
           renderInput={(params) => (
@@ -46,7 +67,7 @@ function App() {
               label="Choose a State"
             />
           )}
-        />
+        />}
         <Stack sx={{margin: "0.5em", marginLeft: "auto"}} direction="row" spacing={1}>
           <h5 id="plan-name">{planName}</h5>
           <AddDistricting />
@@ -59,20 +80,21 @@ function App() {
         Your code can go here.
         This is the empty tab on the left.
         */
-          <InformationTab stateId={currState} planId={planId} setPlanName={setPlanName}/>
+          <InformationTab stateId={currUSstate} planId={planId} setPlanName={setPlanName}/>
         }
         </div>}
         <div style={{ flex: '2' }}>
-          <MapContainer center={[39, -98]} zoom={5} maxBounds={[[5.499550, -167.276413], [83.162102, -52.233040]]}
+          <MapContainer center={[39, -98]} zoom={5} maxBounds={[[5.499550, -167.276413], [83.162102, -52.233040]]} whenCreated={setUSMap}
             // add max zoom
             style={{ height: '100%', width: '100%' }}
           >
             <TileLayer
               url="http://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
             />
-            <GeoJSON
-              data={gdata}
-            />
+            {USstatesGJSON && <GeoJSON
+              data={USstatesGJSON}
+              ref={mapGJSONref}
+            />}
           </MapContainer>
         </div>
       </div>
