3ee6f447a6198010a10486da52985568c5ef64e6
May Me Me Maung <maymememaung27@gmail.com>
2022-05-03 19:51:58 -0400

    replace dummy data with real data

--- src/components/BoxWhisker.js
+++ src/components/BoxWhisker.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
 import createPlotlyComponent from "react-plotly.js/factory";
 import axios from "axios";
-import dummyData from "./placeholder.json";
+// import dummyData from "./placeholder.json";
+import dummyData from "./ALL.json";
+import planData from "./800.json";
 
 const Plotly = window.Plotly;
 const Plot = createPlotlyComponent(Plotly);
 
 export const BoxWhisker = (props) => {
   let planData = {
-    x: [0, 1, 2, 3, 4],
-    y: [1, 1, 2, 3.5, 4.5],
+    x: [0, 1, 2, 3],
+    y: [0.37065, 0.47064, 0.52528, 0.67706],
     name: "Plan 1",
     type: "scatter",
     mode: "markers",
@@ -19,7 +21,30 @@ export const BoxWhisker = (props) => {
     },
   };
 
-  const [category, setCategory] = useState("Republican Population Share");
+  const constructPlanDataPlot = () => {
+    let xrange = [];
+    let yrange = [];
+    planData.forEach((district) => {
+      console.log(district);
+      xrange.push(district["districtId"] - 1);
+      yrange.push(district[category]);
+    });
+    let planDataPlot = {
+      x: xrange,
+      y: yrange,
+      name: "Plan 1",
+      type: "scatter",
+      mode: "markers",
+      marker: {
+        color: "#734E46",
+        opacity: 0.8,
+      },
+    };
+
+    return planDataPlot;
+  };
+
+  const [category, setCategory] = useState("WHITE");
   const [data, setData] = useState(dummyData);
 
   let xaxis = {
@@ -58,6 +83,23 @@ export const BoxWhisker = (props) => {
       });
   }, []);
 
+  // const constructPlotData = (districts) => {
+  //   let plotData = [],
+  //     minArr = [],
+  //     maxArr = [],
+  //     medianArr = [],
+  //     lowerQuartileArr = [],
+  //     upperQuartileArr = [];
+
+  //   for (let districtId in districts) {
+  //     let box = districts[districtId];
+  //     minArr.push(box["min"]);
+  //     maxArr.push(box["max"]);
+  //     medianArr.push(box["median"]);
+  //     lowerQuartileArr.push(box["lowerQuartile"]);
+  //     upperQuartileArr.push(box["upperQuartile"]);
+  //   }
+
   const constructPlotData = (districts) => {
     let plotData = [],
       minArr = [],
@@ -66,14 +108,24 @@ export const BoxWhisker = (props) => {
       lowerQuartileArr = [],
       upperQuartileArr = [];
 
-    for (let districtId in districts) {
-      let box = districts[districtId];
-      minArr.push(box["min"]);
-      maxArr.push(box["max"]);
-      medianArr.push(box["median"]);
-      lowerQuartileArr.push(box["lowerQuartile"]);
-      upperQuartileArr.push(box["upperQuartile"]);
-    }
+    // for (let districtId in districts) {
+    //   let box = districts[districtId];
+    //   console.log(box);
+    //   minArr.push(box["whislo"]);
+    //   maxArr.push(box["whishi"]);
+    //   medianArr.push(box["med"]);
+    //   lowerQuartileArr.push(box["q1"]);
+    //   upperQuartileArr.push(box["q3"]);
+    // }
+
+    districts.forEach((district) => {
+      // console.log(district);
+      minArr.push(district["whislo"]);
+      maxArr.push(district["whishi"]);
+      medianArr.push(district["med"]);
+      lowerQuartileArr.push(district["q1"]);
+      upperQuartileArr.push(district["q3"]);
+    });
 
     let boxTrace = {
       q1: lowerQuartileArr,
@@ -86,6 +138,7 @@ export const BoxWhisker = (props) => {
     };
 
     plotData.push(boxTrace);
+    // plotData.push(constructPlanDataPlot());
     plotData.push(planData);
     return plotData;
   };
